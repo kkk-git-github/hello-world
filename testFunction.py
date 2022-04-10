19902e3ea808c997f38e22bed4ca7e378f6d85ab
Oscar Nguyen <97220895+kkk-git-github@users.noreply.github.com>
2022-04-10 16:52:32 +0700

    Update testFunction.py

--- testFunction.py
+++ testFunction.py
@@ -1,35 +1,17 @@
-import logging
-import random
 import sys
+import random
 
-def count(var):
-  num_var = int(0)
-  if (var):
-    ++num_var
+def cutline(word) :
+  str(word)
+  for i in word:
+    print(i)
 
-  if num_var < 2:
-    print(var, end="")
-    print(" has run ", end="")
-    print(num_var, end="")
-    print(" time.")
-  else:
-    print(var, end="")
-    print(" has run ", end="")
-    print(num_var, end="")
-    print( " times.")
+def function(functionName, expression ,action):
+  if expression == None:
+    pass
+  str(functionName)
+  str(expression)
+  str(action)
 
-random_var = 'hello'
-count(random_var)
-
-random_var
-random_var
-random_var
-
-def turnToFrac(a, b):
-  print(a, end="")
-  print("/", end="")
-  print(b)
-
-turnToFrac(6,7)
-
-sys.exit()
+  def functionName(expression):
+    action
