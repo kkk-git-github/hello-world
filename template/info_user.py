9605367e39164324b1c399e83a6b9b3a7c6a71f8
Oscar Nguyen <97220895+kkk-git-github@users.noreply.github.com>
2022-05-04 18:07:05 +0700

    additional details

--- template/info_user.py
+++ template/info_user.py
@@ -1,27 +1,25 @@
 import logging
-import random
 import sys
 import time
 import datetime
-import traceback
-import threading
 
 class user:
-  email = str()
+  email = str(input("Enter your password: "))
   logging.info("User email (new):" + email)
   if email !=  ("@" and ".com"):
     time.sleep(300)
-    print("Invalid email. Try again: (each session lasts 5 minutes)")
+    print("Invalid email. (each session lasts 5 minutes)")
     print("Require: @ and .com in your email")
     logging.error("Not satisfied email." + datetime.time + "," + datetime.date)
+    sys.exit()
 
-  password = str()
+  password = str(input("Enter your email: "))
   logging.info("User password (new):" + password)
   if len(password) < 8:
     time.sleep(600)
-    print("Invalid password. Try again: (each session lasts 10 minutes)")
+    print("Invalid password. (each session lasts 10 minutes)")
     print("Require: password length should be 8 or greater")
     logging.error("Not satisfied password." + datetime.time + "," + datetime.time)
+    sys.exit()
 
-
-
+usernew1 = user()
