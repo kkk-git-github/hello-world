76d168dc482731d4f1af36a0b45161f6f744e12c
Oscar Nguyen <97220895+kkk-git-github@users.noreply.github.com>
2022-05-04 18:07:05 +0700

    additional details

---
+++ template/template.js
@@ -0,0 +1,7 @@
+var orderType1 = "string";
+var orderType2 = "int";
+function createObject(object, value) {
+    return object = value;
+}
+createObject("mord", "string");
+console.log(createObject("new obj", "new value assigned abstract object"));
