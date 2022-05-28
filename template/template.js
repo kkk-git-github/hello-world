091ea8677a2733dec8232d63927c3224fa745230
Oscar Nguyen <97220895+kkk-git-github@users.noreply.github.com>
2022-05-28 08:24:27 +0700

    update main profile
    Update user's main profile and created stats section for user #8

--- template/template.js
+++
@@ -1,7 +0,0 @@
-var orderType1 = "string";
-var orderType2 = "int";
-function createObject(object, value) {
-    return object = value;
-}
-createObject("mord", "string");
-console.log(createObject("new obj", "new value assigned abstract object"));
